072d2b05f7511ddae18e6e21bf049ed0bcf7e9a9
Isaac Moretão <91755254+IsaacMoretao@users.noreply.github.com>
2023-03-07 17:00:55 -0300

    Commit

--- web/src/pages/Camisas.tsx
+++ web/src/pages/Camisas.tsx
@@ -1,18 +1,40 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { api } from '../lib/axios';
 import { Produto } from './../components/Produto';
 
 export function Camisas(){
 
-  async function userLog(){
-    await api.get('/Dresses/find', {
-      code,
-      
-    }
-  }
+  const [ items, setItems ] = useState();
+
+  useEffect(() => {
+    api
+      .get("/shirt/find")
+      .then((response) => setItems(response.data))
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+  }, []);
+
+/*  useEffect(() => {
+    axios.get('http://localhost:3333/shirt/find')
+    .then(res => {
+      setItems(res.data);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }, [])*/
 
   return(
     <main>
-      <Produto Title={'Camisa'} ProductUrl={'file:///C:/Users/Natanael/Pictures/Camera%20Roll/profile-pic.png'} Description={'lorem'} Code={'CBAY6'}/>
+      {items.map(item => (
+        <div key={item.id}>
+          <Produto Title={''} ProductUrl={'file:///C:/Users/Natanael/Pictures/Camera%20Roll/profile-pic.png'} Description={'lorem'} Code={'CBAY6'}/>
+        </div>
+      ))}
+      
+      
 
     </main>
   )
