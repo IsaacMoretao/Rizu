3ece0920f6af88ea2f7eb73edfc5501caff662fc
Isaac Moretão <91755254+IsaacMoretao@users.noreply.github.com>
2023-03-08 16:38:52 -0300

    Commit

--- web/src/pages/Camisas.tsx
+++ web/src/pages/Camisas.tsx
@@ -3,36 +3,54 @@ import { useEffect, useState } from 'react';
 import { api } from '../lib/axios';
 import { Produto } from './../components/Produto';
 
+interface Shirt {
+  id: string,
+  code: string,
+  title: string,
+  description: string,
+  pieceUrl: string,
+  tipe: string,
+
+  priceInCents: number,
+  quantityPP?: number,
+  quantityP?: number,
+  quantityM?: number,
+  quantityG?: number,
+  quantityGG?: number
+
+}
+
 export function Camisas(){
 
-  const [ items, setItems ] = useState();
+  const [ items, setItems ] = useState<Shirt[]>([]);
 
   useEffect(() => {
     api
       .get("/shirt/find")
       .then((response) => setItems(response.data))
       .catch((err) => {
-        console.error("ops! ocorreu um erro" + err);
-      });
+      console.error("ops! ocorreu um erro" + err);
+    });
   }, []);
 
 /*  useEffect(() => {
-    axios.get('http://localhost:3333/shirt/find')
-    .then(res => {
-      setItems(res.data);
-    })
-    .catch(err => {
-      console.log(err);
-    })
-  }, [])*/
+    async function fetchData() {
+      const result = await getShirts();
+      setShirts(result);
+    }
+
+    fetchData();
+  }, []);
+
+  async function getShirts(): Promise<Shirt[]> {
+    const response = await axios.get('http://localhost:3333/shirt/find');
+    return response.data as Shirt[];
+  }*/
+  console.log(items)
 
   return(
     <main>
-      {items.map(item => (
-        <div key={item.id}>
-          <Produto Title={''} ProductUrl={'file:///C:/Users/Natanael/Pictures/Camera%20Roll/profile-pic.png'} Description={'lorem'} Code={'CBAY6'}/>
-        </div>
-      ))}
+    
       
       
 
