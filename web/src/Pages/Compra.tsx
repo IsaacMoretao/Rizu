7c3bc09e4ae5a9bb98d1e7d0438606e3bbedc58d
Isaac Moretão <91755254+IsaacMoretao@users.noreply.github.com>
2023-04-17 17:05:23 -0300

    Commit

--- web/src/Pages/Compra.tsx
+++ web/src/Pages/Compra.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+import Sorriso from '../assets/icon-sorriso.png'
 
 interface Acessories {
   id: string,
@@ -39,189 +40,227 @@ export function Compra(){
   const [cpf, setCpf] = useState<number | undefined>();
 
   let { id } = useParams();
+  
 
-  const [ items, setItems ] = useState<Acessories[] | null>(null);
-
+  const [ items, setItems ] = useState<Acessories>();
+  const idURL = (items?.id) == id
 
   useEffect(() => {
     fetch(`http://localhost:3333/allParts/${id}`)
     .then(response => response.json())
-    .then((data: Acessories[]) => setItems(data));
+    .then((data) => setItems(data.FindAll));
 }, []);
 
- console.log(items)
-  
- items ? (
-  <ul>
-    {items.map(item => (
-      <li key={item.id}>{item.}</li>
-    ))}
-  </ul>
-) : (
-  <p>Loading...</p>
-)
+  return(  
 
-  return(
-    <main className="bricks center">
+    <main className="bricks main center">
+
+      {idURL? 
+      
       <form className="bg-vinho min-h-[80vh] w-auto p-10 border-yellow border-2 gap-5 m-[100px] text-yellow center flex-col rounded-xl">
-        <strong>COMPRA</strong>
-        <div className="flex gap-3">
+      <strong>COMPRA</strong>
+      <div className="flex gap-3">
+
+        {items?.quantityPP?
           <input
             type="number"
             className="p-2 border-b-2 border-yellow text-center bg-transparent w-[50px]"
             value={quantityPP}
+            max={items?.quantityPP}
             min="0"
             onChange={event => setQuantityPP(event.target.valueAsNumber)}
             placeholder="PP:"
           />
+          :
+          <div/>
+        }
 
+        {items?.quantityP?
           <input
             type="number"
             className="p-2 border-b-2 border-yellow text-center bg-transparent w-[50px]"
             value={quantityP}
+            max={items?.quantityP}
             min="0"
             onChange={event => setQuantityP(event.target.valueAsNumber)}
             placeholder="P:"
           />
+          :
+          <div/>
+        }
 
+        {items?.quantityM?
           <input
             type="number"
             className="p-2 border-b-2 border-yellow text-center bg-transparent w-[50px]"
             value={quantityM}
+            max={items?.quantityM}
             min="0"
             onChange={event => setQuantityM(event.target.valueAsNumber)}
             placeholder="M:"
           />
+          :
+          <div/>
+        }
 
+        {items?.quantityG?
           <input
             type="number"
             className="p-2 border-b-2 border-yellow text-center bg-transparent w-[50px]"
             value={quantityG}
+            max={items?.quantityG}
             min="0"
             onChange={event => setQuantityG(event.target.valueAsNumber)}
             placeholder="G:"
           />
+          :
+          <div/>
+        }
 
+        {items?.quantityGG?
           <input
             type="number"
             className="p-2 border-b-2 border-yellow text-center bg-transparent w-[50px]"
-            min="0"
             value={quantityGG}
+            max={items?.quantityGG}
+            min="0"
             onChange={event => setQuantityGG(event.target.valueAsNumber)}
             placeholder="GG:"
           />
-        </div>
-          <input
-            className='w-[300px] p-2 border-b-2 border-yellow bg-transparent'
-            onChange={event => setGmail(event.target.value)}
-            value={gmail}
-            placeholder='G-mail:'
-            type="email"
-          />
-          <div className='flex gap-3'>
-            <input
-              className='w-[196px] p-2 border-b-2 border-yellow bg-transparent'
-              placeholder='Endereço:'
-              value={endereço}
-              onChange={event => setEndereço(event.target.value)}
-              type="text"
-            />
+          :
+          <div/>
+        }
 
-            <input
-              type="number"
-              className="p-2 border-b-2 border-yellow bg-transparent w-[96px]"
-              min="0"
-              value={numero}
-              onChange={event => setNumero(event.target.valueAsNumber)}
-              placeholder="Numero:"
-            />
-          </div>
 
+      </div>
+        <input
+          className='w-[300px] p-2 border-b-2 border-yellow bg-transparent'
+          onChange={event => setGmail(event.target.value)}
+          value={gmail}
+          placeholder='G-mail:'
+          type="email"
+        />
+        <div className='flex gap-3'>
           <input
+            className='w-[196px] p-2 border-b-2 border-yellow bg-transparent'
+            placeholder='Endereço:'
+            value={endereço}
+            onChange={event => setEndereço(event.target.value)}
             type="text"
-            className="p-2 border-b-2 border-yellow bg-transparent w-[300px]"
-            value={complemento}
-            onChange={event => setComplemento(event.target.value)}
-            placeholder="Complemento:"
           />
 
-          <strong>DADOS DO CARTÂO</strong>
-          <div className='flex gap-3'>
+          <input
+            type="number"
+            className="p-2 border-b-2 border-yellow bg-transparent w-[96px]"
+            min="0"
+            value={numero}
+            onChange={event => setNumero(event.target.valueAsNumber)}
+            placeholder="Numero:"
+          />
+        </div>
 
-            <input
-              className="p-2 border-b-2 border-yellow bg-transparent w-[250px]"
-              type="number"
-              value={numeroDoCartao}
-              onChange={event => setNumeroDoCartao(event.target.valueAsNumber)}
-              placeholder="numero do cartão:"
-            />
+        <input
+          type="text"
+          className="p-2 border-b-2 border-yellow bg-transparent w-[300px]"
+          value={complemento}
+          onChange={event => setComplemento(event.target.value)}
+          placeholder="Complemento:"
+        />
 
-            <input
-              className="p-2 border-b-2 border-yellow bg-transparent w-[250px]"
-              value={nomeNoCartao}
-              onChange={event => setNomeNoCartao(event.target.value)}
-              type="text"
-              placeholder="Nome impresso no cartão:"
-            />
+        <strong>DADOS DO CARTÂO</strong>
+        <div className='flex gap-3'>
 
-          </div>
+          <input
+            className="p-2 border-b-2 border-yellow bg-transparent w-[250px]"
+            type="number"
+            value={numeroDoCartao}
+            onChange={event => setNumeroDoCartao(event.target.valueAsNumber)}
+            placeholder="numero do cartão:"
+          />
+
+          <input
+            className="p-2 border-b-2 border-yellow bg-transparent w-[250px]"
+            value={nomeNoCartao}
+            onChange={event => setNomeNoCartao(event.target.value)}
+            type="text"
+            placeholder="Nome impresso no cartão:"
+          />
+
+        </div>
 
 
-          <div className='flex gap-3 items-end'>
-            <div>
-              validade:
-              <div className='flex gap-1'>
-                <input
-                  type="number"
-                  className="p-2 border-b-2 border-yellow pt-0 text-center bg-transparent w-[50px]"
-                  onChange={event => setMes(event.target.valueAsNumber)}
-                  value={mes}
-                  min="0"
-                  placeholder="mês:"
-                />
-                <input
-                  type="number"
-                  className="p-2 border-b-2 border-yellow pt-0 text-center bg-transparent w-[50px]"
-                  value={ano}
-                  onChange={event => setAno(event.target.valueAsNumber)}
-                  min="0"
-                  placeholder="ano:"
-                />
-              </div>
-
+        <div className='flex gap-3 items-end'>
+          <div>
+            validade:
+            <div className='flex gap-1'>
+              <input
+                type="number"
+                className="p-2 border-b-2 border-yellow pt-0 text-center bg-transparent w-[50px]"
+                onChange={event => setMes(event.target.valueAsNumber)}
+                value={mes}
+                min="0"
+                placeholder="mês:"
+              />
+              <input
+                type="number"
+                className="p-2 border-b-2 border-yellow pt-0 text-center bg-transparent w-[50px]"
+                value={ano}
+                onChange={event => setAno(event.target.valueAsNumber)}
+                min="0"
+                placeholder="ano:"
+              />
             </div>
-          <input
-            type="number"
-            className="p-2 border-b-2 border-yellow bg-transparent  w-[300px]"
-            value={codigoSegurança}
-            onChange={event => setCodigoSegurança(event.target.valueAsNumber)}
-            min="0"
-            placeholder="Código de segurança:"
-          />
+
           </div>
-            
+        <input
+          type="number"
+          className="p-2 border-b-2 border-yellow bg-transparent  w-[300px]"
+          value={codigoSegurança}
+          onChange={event => setCodigoSegurança(event.target.valueAsNumber)}
+          min="0"
+          placeholder="Código de segurança:"
+        />
+        </div>
+          
+        <input
+          type="number"
+          onChange={event => setCpf(event.target.valueAsNumber)}
+          value={cpf}
+          className="p-2 border-b-2 border-yellow bg-transparent w-[300px]"
+          min="0"
+          placeholder="CPF do titular"
+        />
 
-
-          <input
-            type="number"
-            onChange={event => setCpf(event.target.valueAsNumber)}
-            value={cpf}
-            className="p-2 border-b-2 border-yellow bg-transparent w-[300px]"
-            min="0"
-            placeholder="CPF do titular"
-          />
-
-        <button
-          className="
-          border-cyan-400 border-2 bg-clip-text text-lg font-bold text-transparent
-          bg-gradient-to-r from-blue-500 to-blue-400 px-10 py-2 rounded-full">
-          COMPRAR
-        </button>
+      <button
+        className="
+        border-cyan-400 border-2 bg-clip-text text-lg font-bold text-transparent
+        bg-gradient-to-r from-blue-500 to-blue-400 px-10 py-2 rounded-full">
+        COMPRAR
+      </button>
 
 
 
 
       </form>
+      
+      :
+
+      <div className="
+        bg-vinho p-16 border-yellow border-2 text-xl
+        text-yellow center flex-col rounded-xl">
+
+        <img src={Sorriso} className='h-[200px]' />
+        <p>Clecione o uma peça para poder realizar a compra</p>
+        <Link
+          to={"/"}
+          className="border-2 mt-5 border-colorTxt text-colorTxt rounded-lg px-10 py-2">
+          Entrar
+        </Link>
+
+      </div >
+
+      }
+      
     </main>
   )
 }
